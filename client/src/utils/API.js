6812d9dec14c74b49e27f9e1d02733471b5817f9
arm <moraveca@gmail.com>
2019-08-06 10:14:17 -0500

    changed calls to heroku, yarn upgrade

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,10 +3,10 @@ import axios from "axios";
 export default {
   // Gets the product with the given id
   getProduct: id => {
-    return axios.get("/api/products/" + id)
+    return axios.get("https://stark-bastion-48445.herokuapp.com/api/products/" + id)
   },
 
   updatePrice: (id, newPrice) => {
-    return axios.put("/api/products/" + id + "/" + newPrice)
+    return axios.put("https://stark-bastion-48445.herokuapp.com/api/products/" + id + "/" + newPrice)
   }
 };
